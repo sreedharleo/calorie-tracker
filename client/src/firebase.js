c5687acdee6cc1dcf14bcd08fad51d8620a970dc
sreeleo <rsreedhar103@gmail.com>
2026-02-03 11:36:46 +0530

    database issue

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "calorie-tracker-aad75.firebasestorage.app",
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
